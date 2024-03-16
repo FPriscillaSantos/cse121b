c62bb6996745e0e247d044adf8c8173f32e090f2
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-03-16 17:13:28 -0300

    done

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -19,11 +19,13 @@
     document.getElementById("sum").value = sum;
   });*/
 
-  function add(number1, number2) {
+  function add(number1, number2) 
+  {
     return number1 + number2;
   }
 
-  function addNumbers() {
+  function addNumbers() 
+  {
     let addNumber1 = Number(document.querySelector('#add1').value);
     let addNumber2 = Number(document.querySelector('#add2').value);
     document.querySelector('#sum').value = add(addNumber1, addNumber2);
@@ -38,7 +40,8 @@
     return number1 - number2;
   }
 
-  const subtractNumbers = function() {
+  const subtractNumbers = function() 
+  {
     let subtractNumber1 = Number(document.querySelector('#subtract1').value);
     let subtractNumber2 = Number(document.querySelector('#subtract2').value);
     document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
@@ -50,7 +53,8 @@
 /* Arrow Function - Multiply Numbers */
   const multiply = (number1, number2) => number1 * number2;
 
-  const multiplyNumbers = () => {
+  const multiplyNumbers = () => 
+  {
     let multiplyNumber1 = Number(document.querySelector('#factor1').value);
     let multiplyNumber2 = Number(document.querySelector('#factor2').value);
     document.querySelector('#product').value = multiply(multiplyNumber1, multiplyNumber2);
@@ -60,11 +64,13 @@
 
 
 /* Open Function Use - Divide Numbers */
-function divide(number1, number2) {
+function divide(number1, number2) 
+  {
     return number1 / number2;
   }
 
-  function divideNumbers() {
+  function divideNumbers() 
+  {
     let divideNumber1 = Number(document.querySelector('#dividend').value);
     let divideNumber2 = Number(document.querySelector('#divisor').value);
     document.querySelector('#quotient').value = divide(divideNumber1, divideNumber2);
@@ -74,17 +80,39 @@ function divide(number1, number2) {
 
 
 /* Decision Structure */
+  document.querySelector('#getTotal').addEventListener('click', getTotalDue);
 
+  function getTotalDue() 
+  { 
+  let subtotal = parseFloat(document.querySelector('#subtotal').value);
+
+  //desconto de 20% se for um membro
+  let isMember = document.querySelector('#member').checked;
+  if (isMember) 
+  {
+      subtotal *= 0.8; 
+  }
+
+  document.querySelector('#total').textContent = `$ ${subtotal.toFixed(2)}`;
+}
+
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
+document.querySelector('#array').textContent = numbersArray;
 
 /* Output Odds Only Array */
+document.querySelector('#odds').textContent = numbersArray.filter(number => number % 2 !== 0);
 
 /* Output Evens Only Array */
+document.querySelector('#evens').textContent = numbersArray.filter(number => number % 2 === 0);
 
 /* Output Sum of Org. Array */
+document.querySelector('#sumOfArray').textContent = numbersArray.reduce((sum, number) => sum + number);
 
 /* Output Multiplied by 2 Array */
+document.querySelector('#multiplied').textContent = numbersArray.map(number => number * 2);
 
 /* Output Sum of Multiplied by 2 Array */
+document.querySelector('#sumOfMultiplied').textContent = numbersArray.map(number => number * 2).reduce((sum, number) => sum + number);
