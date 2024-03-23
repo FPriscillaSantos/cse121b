3cdbe6a810aa483a225c4ded70a9098b571fc759
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-03-23 14:22:21 -0300

    parei no passo 4

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -3,7 +3,9 @@
 /* Profile Object  */
 let myProfile = {
     name: "F Priscilla Santos",
+
     photo: 'images/profile index image.jpeg',
+
     favoriteFood: [
         'Milk pudding', 
         'Blackberry', 
@@ -11,28 +13,40 @@ let myProfile = {
         'Bean', 
         'Strawberry'
     ],
+
     hobbies: [
         'Watch K-dramas and Run BTS',
         'To read',
         'Listen to music',
         'To dance',
         'Play with my children'
-    ]
+    ],
+
+    placesLived:[],
 }
-
-
-
 /* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+    {
+        place: 'Ribeirão Preto, SP',
+        length: '25 years'
+    }
+);
 
-
-
+myProfile.placesLived.push(
+    {
+      place: 'São Paulo, SP',
+      length: '5 anos'
+    }
+  );
 
 /* DOM Manipulation - Output */
-
 /* Name */
+document.querySelector('#name').textContent =  myProfile.name;
 
 /* Photo with attributes */
-
+let image = document.querySelector('#photo');
+image.setAttribute('src', myProfile.photo);
+image.setAttribute('alt', myProfile.name);
 
 /* Favorite Foods List*/
 
