1d74227113dc8aeb163f6785f29c775671e7afee
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-03-23 14:02:00 -0300

    2

---
+++ scripts/w04-task.js
@@ -0,0 +1,45 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "F Priscilla Santos",
+    photo: 'images/profile index image.jpeg',
+    favoriteFood: [
+        'Milk pudding', 
+        'Blackberry', 
+        'Tapioca Biscuit', 
+        'Bean', 
+        'Strawberry'
+    ],
+    hobbies: [
+        'Watch K-dramas and Run BTS',
+        'To read',
+        'Listen to music',
+        'To dance',
+        'Play with my children'
+    ]
+}
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
